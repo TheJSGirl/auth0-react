47b430f83e797e952a1c91079c05103b6841cc97
thejsgirl <prinegi100@gmail.com>
2019-07-15 00:08:24 +0530

    feat: added initalState

--- src/index.js
+++ src/index.js
@@ -7,9 +7,14 @@ import * as serviceWorker from './serviceWorker';
 let state = {};
 window.setState = (changes) => {
     state = Object.assign({}, state, changes);
-    ReactDOM.render(<App />, document.getElementById('root'));
+    ReactDOM.render(<App {...state}/>, document.getElementById('root'));
 }
 
+let initialState = {
+    "name": "Priyanka"
+}
+
+window.setState(initialState)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
